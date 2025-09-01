b4a9c3987a41236231b9474481fe302afbab6e11
mmddmmm <mmddmmm.real@gmail.com>
2025-09-01 16:03:51 -0700

    Create steal anime.lua

---
+++ Realgui/steal anime.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mmddmmm/brookhaven/refs/heads/main/Realgui/fly.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mmddmmm/brookhaven/refs/heads/main/Realgui/tp%20tools.txt"))()
